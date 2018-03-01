e0fe753caa25a67094be6077b8523c6552a099de
Bryn Bell <Bryn@bryns-mbp-2.home>
2018-02-28 20:23:36 -0500

    changed class attribute

--- Models/Workout.cs
+++ Models/Workout.cs
@@ -1,11 +1,12 @@
 using System;
+using System.Collections.Generic;
 
 namespace Exercises.Api.Models
 {
     public class Workout
     {
       public int Id { get; set; }
-      public Exercise[] Exercise { get; set; }
+      public List<Exercise> Exercise { get; set; }
 
       public DateTime WorkoutDate { get; set; }
     }
