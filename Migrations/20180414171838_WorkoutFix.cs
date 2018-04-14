c487b6d937d500d7e53cd7d036998d15eb779988
wohnjoods <wohnjoods@gmail.com>
2018-04-14 13:19:10 -0400

    asdf

---
+++ Migrations/20180414171838_WorkoutFix.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Exercises.Api.Migrations
+{
+    public partial class WorkoutFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProgramWorkouts_Workouts_workoutId",
+                table: "ProgramWorkouts",
+                column: "workoutId",
+                principalTable: "Workouts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProgramWorkouts_Workouts_workoutId",
+                table: "ProgramWorkouts");
+        }
+    }
+}
